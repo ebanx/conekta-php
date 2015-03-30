4bf7f5653850526647175ea5d528801fcce0b6af
DanX <danx.exe@gmail.com>
2015-03-30 16:59:31 -0300

    Set HTTP error code and API error code on exception class

--- lib/Conekta/Error.php
+++ lib/Conekta/Error.php
@@ -1,42 +1,44 @@
 <?php
 class Conekta_Error extends Exception
 {
-	public function __construct($message=null, $message_to_purchaser=null, $type=null, $code=null, $params=null) {
+	public function __construct($message=null, $message_to_purchaser=null, $type=null, $http_code=null, $code=null, $params=null) {
 		parent::__construct($message);
 		$this->message = $message;
 		$this->message_to_purchaser = $message_to_purchaser;
 		$this->type = $type;
+		$this->http_code = $http_code;
 		$this->code = $code;
 		$this->params = $params;
 	}
 	
-	public static function errorHandler($resp, $code) {
+	public static function errorHandler($resp, $http_code) {
 		$resp = json_decode($resp, true);
 		$message = isset($resp['message']) ? $resp['message'] : null;
 		$message_to_purchaser = isset($resp['message_to_purchaser']) ? $resp['message_to_purchaser'] : null;
 		$type = isset($resp['type']) ? $resp['type'] : null;
 		$params = isset($resp['param']) ? $resp['param'] : null;
-		if (isset($code) != true || $code == 0) {
+		$code = isset($resp['code']) ? $resp['code'] : null;
+		if (isset($http_code) != true || $http_code == 0) {
 			throw new Conekta_NoConnectionError(
 				LANG::translate('error.requestor.connection', array('BASE'=>Conekta::$apiBase), LANG::EN),
 				LANG::translate('error.requestor.connection_purchaser', null, Conekta::$locale),
 				$type, $code, $params);
 		}
-		switch ($code) {
+		switch ($http_code) {
 			case 400:
-				throw new Conekta_MalformedRequestError($message, $message_to_purchaser, $type, $code, $params);
+				throw new Conekta_MalformedRequestError($message, $message_to_purchaser, $type, $http_code, $code, $params);
 			case 401:
-				throw new Conekta_AuthenticationError($message, $message_to_purchaser, $type, $code, $params);
+				throw new Conekta_AuthenticationError($message, $message_to_purchaser, $type, $http_code, $code, $params);
 			case 402:
-				throw new Conekta_ProcessingError($message, $message_to_purchaser, $type, $code, $params);
+				throw new Conekta_ProcessingError($message, $message_to_purchaser, $type, $http_code, $code, $params);
 			case 404:
-				throw new Conekta_ResourceNotFoundError($message, $message_to_purchaser, $type, $code, $params);
+				throw new Conekta_ResourceNotFoundError($message, $message_to_purchaser, $type, $http_code, $code, $params);
 			case 422:
-				throw new Conekta_ParameterValidationError($message, $message_to_purchaser, $type, $code, $params);
+				throw new Conekta_ParameterValidationError($message, $message_to_purchaser, $type, $http_code, $code, $params);
 			case 500:
-				throw new Conekta_ApiError($message, $message_to_purchaser, $type, $code, $params);
+				throw new Conekta_ApiError($message, $message_to_purchaser, $type, $http_code, $code, $params);
 			default:
-				throw new Conekta_Error($message, $message_to_purchaser, $type, $code, $params);
+				throw new Conekta_Error($message, $message_to_purchaser, $type, $http_code, $code, $params);
 		}
 	}
 }
